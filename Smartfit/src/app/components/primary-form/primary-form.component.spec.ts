564879ee83a10fb2e1ac9a22015cc2ffaeaf1d31
Marcos Antônio <marcosjunior.me.009@gmail.com>
2024-05-09 19:17:34 -0300

    feat: adicionando os formulários

---
+++ Smartfit/src/app/components/primary-form/primary-form.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PrimaryFormComponent } from './primary-form.component';
+
+describe('PrimaryFormComponent', () => {
+  let component: PrimaryFormComponent;
+  let fixture: ComponentFixture<PrimaryFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PrimaryFormComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(PrimaryFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Smartfit/src/app/components/primary-form/primary-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-primary-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './primary-form.component.html',
+  styleUrl: './primary-form.component.scss'
+})
+export class PrimaryFormComponent {
+ result: [] = []
+}
