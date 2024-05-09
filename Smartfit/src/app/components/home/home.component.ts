6e429454ad013c8b7d92e3ccf4b9256993302843
Marcos Antônio <marcosjunior.me.009@gmail.com>
2024-05-09 19:19:13 -0300

    feat: adicionando a home

--- Smartfit/src/app/components/home/home.component.ts
+++ Smartfit/src/app/components/home/home.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { PrimaryFormComponent } from '../primary-form/primary-form.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent,
+    PrimaryFormComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
