9fb9cef2204c30421d6a121a9fff223a5c3e1577
Marcos Antônio <marcosjunior.me.009@gmail.com>
2024-05-10 19:05:57 -0300

    feat: arrumando responsividade e componentes

--- Smartfit/src/app/components/home/home.component.ts
+++ Smartfit/src/app/components/home/home.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { PrimaryFormComponent } from '../primary-form/primary-form.component';
+import { LegendBarComponent } from '../legend-bar/legend-bar.component';
+import { FilteredSearchComponent } from '../filtered-search/filtered-search.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     HeaderComponent,
-    PrimaryFormComponent
+    PrimaryFormComponent,
+    LegendBarComponent,
+    FilteredSearchComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
